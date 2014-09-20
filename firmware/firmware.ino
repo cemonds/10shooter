19920990a9f8617e024e9f9dc868af24822ab992
Christoph Emonds <ich@christoph-emonds.de>
2014-09-20 02:25:58 +0200

    - blub

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,9 +9,12 @@ AccelStepper stepper(AccelStepper::DRIVER, PIN_STEPPER_STEP, PIN_STEPPER_DIRECTI
 int readByte = 0;
 int nextDrink = 0;
 int currentDrink = 0;
-const long MICRO_STEPS = 2;
-long DrinkPositionFaktor = 20*MICRO_STEPS*15;
+const long INITIAL_POSITION_OFFSET = -960;
+const long MICRO_STEPS = 8;
+long DrinkPositionFaktor = 20*MICRO_STEPS*15-32;
 long targetPosition = 0;
+
+boolean initialized = true;
 boolean waitingForNewPosition = false;
 boolean readyForNextCommand = false;
 
@@ -20,7 +23,6 @@ int pos = 0;
 int servoDirection = 1;
 
 boolean isTapping = false;
-
 const int buttonPin = 5;
 int buttonState = 0;
 boolean init_pos = false;
@@ -46,45 +48,26 @@ void setup()
 void loop()
 {
        buttonState = digitalRead(buttonPin);
-       if (init_pos == false && lastButtonPress == 0)
+       if (init_pos == false)
        {
+           stepper.setSpeed(200*MICRO_STEPS);
+           stepper.runSpeed();
+           init_pos=false;
            if (buttonState == HIGH)
            {
-               if(lastButtonPress == 0) 
+               if(stepper.currentPosition() > 3000) 
                {
-                   stepper.setSpeed(0);
-                   stepper.runSpeed();
-                   stepper.setCurrentPosition(0);
-                   init_pos=true;
-                   readyForNextCommand = true;
-                   stepper.disableOutputs();
+                 stepper.setCurrentPosition(INITIAL_POSITION_OFFSET);
+                 stepper.moveTo(0);
+                 targetPosition = 0;
+                 init_pos=true;
+                 waitingForNewPosition = true;
+                 readyForNextCommand = false;
                }
-           } else {
-               stepper.setSpeed(500);
-               stepper.runSpeed();
-               init_pos=false;
-           }
-             
-       } else {
-           if (buttonState == HIGH)
-           {
-               if(lastButtonPress != 0) 
-               {
-                   if(millis() - lastButtonPress > 1000)
-                   {
-                      init_pos = false; 
-                   }
-               } else {
-                   lastButtonPress = millis();
-               }   
-           } else {
-               lastButtonPress = 0;
            }
        }
 
-
-
-
+    if(initialized) {
         if (Serial.available() > 0 && readyForNextCommand) {
                 readByte = Serial.read();
                 if(readByte == 't') {
@@ -97,15 +80,15 @@ void loop()
                   int drinkDeltaRight = (nextDrink - currentDrink + 10) % 10;
                   int drinkDeltaLeft = (nextDrink - currentDrink - 10) % 10;
                   long stepDelta = 0;
-                  if(abs(drinkDeltaRight) < abs(drinkDeltaLeft)) {
+//                  if(abs(drinkDeltaRight) < abs(drinkDeltaLeft)) {
                     stepDelta = drinkDeltaRight * DrinkPositionFaktor;
-                  } else {
-                    stepDelta = drinkDeltaLeft * DrinkPositionFaktor;
-                  }
+//                  } else {
+//                    stepDelta = drinkDeltaLeft * DrinkPositionFaktor;
+//                  }
                   targetPosition += stepDelta;
                   Serial.print("Target position: ");
                   Serial.println(targetPosition);
-                  stepper.enableOutputs();
+//                  stepper.enableOutputs();
                   stepper.moveTo(targetPosition);
                   waitingForNewPosition = true;
                   readyForNextCommand = false;
@@ -115,18 +98,45 @@ void loop()
                   Serial.print("My Current Position: ");
                   Serial.println(stepper.currentPosition()/DrinkPositionFaktor);
                 } else if(readByte == 'i') {
-                  stepper.enableOutputs();
+//                  stepper.enableOutputs();
                   Serial.println("Searching 0 - Position... ");
+                  stepper.setCurrentPosition(0);
                   init_pos=false;
                   readyForNextCommand = false;
+                } else if(readByte == 'ms') {
+                  long stepDelta = Serial.parseInt();
+                  targetPosition += stepDelta;
+                  Serial.print("Target position: ");
+                  Serial.println(targetPosition);
+//                  stepper.enableOutputs();
+                  stepper.moveTo(targetPosition);
+                  waitingForNewPosition = true;
+                  readyForNextCommand = false;
+                } else if(readByte == 's') {
+                  int angle = Serial.parseInt();
+                  tapServo.write(angle);
                }
           }
+    } else {
+         if( Serial.read() == '1') 
+         if( Serial.read() == '0')
+         if( Serial.read() == 's')
+         if( Serial.read() == 'h')
+         if( Serial.read() == 'o')
+         if( Serial.read() == 'o')
+         if( Serial.read() == 't')
+         if( Serial.read() == 'e')
+         if( Serial.read() == 'r') 
+             initialized = true;
+         
+    }
+     
           if(targetPosition == stepper.currentPosition() && waitingForNewPosition) {
             Serial.println("Drink reached");
             currentDrink = nextDrink;
             waitingForNewPosition = false;
             readyForNextCommand = true;
-            stepper.disableOutputs();
+//            stepper.disableOutputs();
           }
           if(isTapping) {
             Serial.println("Start tapping");
@@ -135,7 +145,25 @@ void loop()
             Serial.println("Stop tapping");
             tapServo.write(0);
             Serial.println("Waiting for refill");
-            delay(4000);
+            tapServo.write(0);
+            delay(500);
+            tapServo.write(90);
+            delay(200);
+            tapServo.write(130);
+            delay(200);
+            tapServo.write(90);
+            delay(200);
+            tapServo.write(50);
+            delay(200);
+            tapServo.write(90);
+            delay(200);
+            tapServo.write(80);
+            delay(800);
+            tapServo.write(60);
+            delay(800);
+            tapServo.write(80);
+            delay(2000);
+            tapServo.write(0);
             isTapping = false;
             readyForNextCommand = true;
           }
